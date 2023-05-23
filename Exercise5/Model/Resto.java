d22e1c4433ca2174e679c2f13746b0e856958bb3
Jewel Paira <119938577+jewelcssndr@users.noreply.github.com>
2023-05-23 12:07:12 +0800

    Create Resto.java

---
+++ Exercise5/Model/Resto.java
@@ -0,0 +1,24 @@
+package Model;
+
+class Resto {
+
+    private String price;
+    private String taste;
+
+    public String getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getTaste() {
+        return this.taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+    
+}
